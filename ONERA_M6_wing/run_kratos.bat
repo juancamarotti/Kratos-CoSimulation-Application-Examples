19f87a8c6f14ba86bda1ec160402c3545f8af6b2
Juan Ignacio Camarotti <juanicama7@gmail.com>
2024-08-16 14:22:38 +0200

    Delete ONERA_M6_wing directory

--- ONERA_M6_wing/run_kratos.bat
+++
@@ -1,8 +0,0 @@
-:: Adds the kratos installation path temporaryly to environmental variable "PATH"
-@echo off
-
-:: Runs kratos with the input file
-..\kratos_main\kratos_main.exe
-
-:: Keep console open
-rem pause
